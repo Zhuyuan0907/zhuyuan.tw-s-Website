352c4152966a3fea66cbf1074161910f946e9d94
Zhuyuan <neiljiang064580@gmail.com>
2026-04-06 12:23:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 const engineers = [
   {
-    name: "工程師姓名 01",
-    tag: "gravatar slot",
-    // 將下面的 REPLACE_WITH_MD5_HASH 替換成對應 email 的 MD5 hash
-    gravatar: "https://www.gravatar.com/avatar/REPLACE_WITH_MD5_HASH?d=identicon&s=320",
+    name: "柴柴",
+    tag: "fallen engineer",
+    gravatar:
+      "https://gravatar.com/avatar/cacb91a081291126489ba6b0e98662c698188ecce6391007b171461b21ef4241?s=512&d=https://sitcon.org/2022/imgs/deafult_avatar.jpg&r=g",
   },
   {
-    name: "工程師姓名 02",
-    tag: "gravatar slot",
-    gravatar: "https://www.gravatar.com/avatar/REPLACE_WITH_MD5_HASH?d=identicon&s=320",
+    name: "康喔",
+    tag: "fallen engineer",
+    gravatar:
+      "https://gravatar.com/avatar/08b25778f9a9ca3f2591b4e859be426dbd075608d763daf4fdd244d48eb26f13?s=512&d=https://sitcon.org/2022/imgs/deafult_avatar.jpg&r=g",
   },
   {
-    name: "工程師姓名 03",
-    tag: "gravatar slot",
-    gravatar: "https://www.gravatar.com/avatar/REPLACE_WITH_MD5_HASH?d=identicon&s=320",
+    name: "鯨魚",
+    tag: "fallen engineer",
+    gravatar:
+      "https://gravatar.com/avatar/190cb3efdc528ba6d3879559d3650bf5f8f555a7f2fbbd7f4e62e0662124bfdc?s=512&d=https://sitcon.org/2022/imgs/deafult_avatar.jpg&r=g",
   },
   {
-    name: "工程師姓名 04",
-    tag: "gravatar slot",
-    gravatar: "https://www.gravatar.com/avatar/REPLACE_WITH_MD5_HASH?d=identicon&s=320",
+    name: "Nelson",
+    tag: "fallen engineer",
+    gravatar:
+      "https://gravatar.com/avatar/014f25869988e740766b2247d49013b50118675df2d0918f9f4f86e565732a3b?s=512&d=https://sitcon.org/2022/imgs/deafult_avatar.jpg&r=g",
   },
   {
-    name: "工程師姓名 05",
-    tag: "gravatar slot",
-    gravatar: "https://www.gravatar.com/avatar/REPLACE_WITH_MD5_HASH?d=identicon&s=320",
-  },
-  {
-    name: "工程師姓名 06",
-    tag: "gravatar slot",
-    gravatar: "https://www.gravatar.com/avatar/REPLACE_WITH_MD5_HASH?d=identicon&s=320",
+    name: "Kevinowo",
+    tag: "fallen engineer",
+    gravatar:
+      "https://gravatar.com/avatar/131cf4dbacc120ab9ce94caea81cb4d835a6efca71b12e2c64452746affba7ba?s=512&d=https://sitcon.org/2022/imgs/deafult_avatar.jpg&r=g",
   },
 ];
 
